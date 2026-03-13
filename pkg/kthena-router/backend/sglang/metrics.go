b4121d88027efc42112b7c0650c3453702c1eb95
katara-Jayprakash <katarajayprakash@icloud.com>
2026-03-14 01:41:57 +0530

    implemented the GetPodModels sglang feature in kthena
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- pkg/kthena-router/backend/sglang/metrics.go
+++ pkg/kthena-router/backend/sglang/metrics.go
@@ -17,7 +17,10 @@ limitations under the License.
 package sglang
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
 	dto "github.com/prometheus/client_model/go"
 	corev1 "k8s.io/api/core/v1"
@@ -54,6 +57,14 @@ var (
 		TTFT:              utils.TTFT,
 	}
 )
+
+type Model struct {
+	ID string `json:"id"`
+}
+
+type ModelList struct {
+	Data []Model `json:"data"`
+}
 
 type sglangEngine struct {
 	// The address of sglang's query metrics is http://{model server}:MetricPort/metrics
@@ -126,7 +137,33 @@ func (engine *sglangEngine) GetHistogramPodMetrics(allMetrics map[string]*dto.Me
 	return wantMetrics, histogramMetrics
 }
 
-// TODO： Methods to get Models from sglang
+// implemented the get models function for sglang engine, 
 func (engine *sglangEngine) GetPodModels(pod *corev1.Pod) ([]string, error) {
-	return nil, nil
+	url := fmt.Sprintf("http://%s:%d/v1/models", pod.Status.PodIP, engine.MetricPort)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get models from pod %s/%s: HTTP %d", pod.GetNamespace(), pod.GetName(), resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var modelList ModelList
+	err = json.Unmarshal(body, &modelList)
+	if err != nil {
+		return nil, err
+	}
+
+	models := make([]string, 0, len(modelList.Data))
+	for _, model := range modelList.Data {
+		models = append(models, model.ID)
+	}
+	return models, nil
 }
